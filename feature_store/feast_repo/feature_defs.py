f2ca37ae6bd879d6157ec614a34b9d5e0d103568
abd96 <abdulhamed.chribati@tu-dortmund.de>
2025-04-21 12:11:40 +0200

    finally fixing dependencies -> working DAG

--- feature_store/feast_repo/feature_defs.py
+++ feature_store/feast_repo/feature_defs.py
@@ -8,16 +8,16 @@ user = Entity(name="user_id", join_keys=["user_id"])
 
 # 2. Define where the data lives
 user_stats_source = FileSource(
-    path="../../data/generated/user_features.parquet",
+    path="data/generated/user_features.parquet",
     timestamp_field="event_timestamp"
 )
 
 
-# 3. Define the FeatureView (your actual features)
+# 3. Define the FeatureView
 user_stats_fv = FeatureView(
     name="user_profile",  # feature group name
     entities=[user],
-    ttl=timedelta(days=1),  # features older than 1 day are stale
+    ttl=timedelta(days=1),  
     schema=[
         Field(name="click_count", dtype=Int64),
         Field(name="days_since_last_active", dtype=Int64)
